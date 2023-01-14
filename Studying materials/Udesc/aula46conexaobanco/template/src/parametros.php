1b262d61fc159fd1cfb7f6f5b7c2b0f33a24b468
lucasclivati <lucascli@hotmail.com>
2023-01-14 13:33:42 -0300

    Update parametros.php

--- Studying materials/Udesc/aula46conexaobanco/template/src/parametros.php
+++ Studying materials/Udesc/aula46conexaobanco/template/src/parametros.php
@@ -1,6 +1,6 @@
 <?php
 $dadosPdo = [
-    'dns' => 'mysql:host=sql206.byetcluster.com;dbname=epiz_33214230_db1;charset=utf8',
-    'usuario' => 'epiz_33214230',
-    'senha' => '87c6vq1x'
+    'dns' => 'mysql:host=sql202.epizy.com;dbname=epiz_33185930_aula_46;charset=utf8',
+    'usuario' => 'epiz_33185930',
+    'senha' => 'incluirsenha'
 ];?>
