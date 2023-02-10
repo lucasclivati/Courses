0679e3a7b2e3dec3804ad88bd5f6a98e459a21dc
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-02-10 17:49:54 -0300

    Update main.js

--- Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
+++ Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
@@ -1,6 +1,5 @@
-function tocaSomPom (){
+function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom');
-
+const listaDeTeclas = document.querySelectorAll('.tecla')
