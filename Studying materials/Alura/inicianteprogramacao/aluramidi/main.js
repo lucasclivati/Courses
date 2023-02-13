cd4d48a1d898b2cea4ea9e70f6b5a7720762e473
lucasclivati <lucascli@hotmail.com>
2023-02-13 00:27:38 -0300

    changed to for

--- Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
+++ Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
@@ -1,14 +1,38 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
+// let contador = 0;
+// while (contador < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`;
+//     tecla.onclick = function(){
+//         tocaSom(idAudio)
+//     }
+//     contador += 1
+//     console.log(contador);
+// }
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    contador += 1
-    console.log(contador);
-}
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+    tecla.onkeydown = function (evento){
+        console.log(evento.code)
+        if(''){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+};
