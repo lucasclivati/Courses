03cf838e9d63531b98371ee9e4bd8865028d43f4
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-02-20 11:14:38 -0300

    Update main.js

--- Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
+++ Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
@@ -25,6 +25,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
+
     tecla.onkeydown = function (evento){
         console.log(evento.code)
         if(evento.code === 'Space'){
@@ -32,6 +33,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         }
         
     }
+    
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa');
     }
