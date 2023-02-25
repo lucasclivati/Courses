56ce8f55b29ecc9006f616525a9232fcbb49539b
lucasclivati <lucascli@hotmail.com>
2023-02-25 18:35:45 -0300

    finished this course aluramidi

--- Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
+++ Studying materials/Alura/inicianteprogramacao/aluramidi/main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
@@ -27,8 +36,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
     tecla.onkeydown = function (evento){
-        console.log(evento.code)
-        if(evento.code === 'Space'){
+        //console.log(evento.code) //
+        if(evento.code === 'Space' || evento.code === 'Enter'){
             tecla.classList.add('ativa');
         }
         
