4aa94b6234d1150c5d424a4199e79168a1f11154
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-04-12 10:54:15 -0300

    aula1 completa

--- Studying materials/Alura/javascriptFrontEnd/mochila-de-viagem/js/main.js
+++ Studying materials/Alura/javascriptFrontEnd/mochila-de-viagem/js/main.js
@@ -1,1 +1,22 @@
-document.getElementById("novoItem")
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criaElemento(nome, quantidade) {
+console.log(nome)
+console.log(quantidade)
+const novoItem = document.createElement('li')
+novoItem.classList.add("item")
+
+const numeroItem = document.createElement('strong')
+numeroItem.innerHTML = quantidade
+
+novoItem.appendChild(numeroItem)
+novoItem.innerHTML += nome
+
+lista.appendChild(novoItem)
+}
